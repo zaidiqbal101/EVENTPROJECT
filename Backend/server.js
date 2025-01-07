0296e5f14a34b8ae149e52ef0820a7066286e48e
Mohd Alquama <mohdalquama1@gmail.com>
2025-01-07 18:44:53 +0530

    model of gallery

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import multer from 'multer';
-import dotenv from 'dotenv';
-import MongoDB from './models/db.js';
-import ContactDataGet from './controllers/contactDataGet.js';
-import InquiriesDataGet from './controllers/inquiriesDataGet.js';
+import express from "express";
+import multer from "multer";
+import dotenv from "dotenv";
+import MongoDB from "./models/db.js";
+import ContactDataGet from "./controllers/contactDataGet.js";
+import InquiriesDataGet from "./controllers/inquiriesDataGet.js";
 dotenv.config(); // Load environment variables from a .env file
 
 const PORT = process.env.PORT || 1200;
@@ -17,44 +17,34 @@ MongoDB({ url: dbUrl });
 app.use(express.json());
 
 // Simple route for testing
-app.get('/', (req, res) => {
-  res.send('Welcome to my Node.js server!');
+app.get("/", (req, res) => {
+  res.send("Welcome to my Node.js server!");
 });
 
 // API route to fetch all documents from the "contactinquiries" collection
-app.get('/contacts', ContactDataGet);
-app.get('/inquiries',InquiriesDataGet);
-
-
-
+app.get("/contacts", ContactDataGet);
+app.get("/inquiries", InquiriesDataGet);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'assets')
+    cb(null, "assets");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
-  }
-})
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
+  },
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
-
-app.post('/upload', upload.fields([
-  { name: '1', maxCount: 1 },
-  { name: '2', maxCount: 1 },
-  { name: '3', maxCount: 1 },
-  { name: '4', maxCount: 1 },
-]), async (req, res) => {
-
-   console.log(req.body);
+app.post("/gallery",upload.fields([{ name: "img", maxCount: 1 }]), async (req, res) => {
    
-  });
+  }
+);
 
 // 404 handler - must be defined **after** all routes
 app.use((req, res) => {
-  res.status(404).send('Route not found');
+  res.status(404).send("Route not found");
 });
 
 // Start the server
